cbf7688902084e874f70ee524fe4f9c416dd3342
Vishnu <44140759+vishnudk@users.noreply.github.com>
2020-08-15 12:07:55 +0530

    Add files via upload

--- message.py
+++ message.py
@@ -26,12 +26,17 @@ def function():
     time.sleep(5)
     button.send_keys(Keys.ENTER)
     time.sleep(5)
+    count=0
     # while True:
         # messageBar = driver.find_element_by_xpath("//div[@class='_3uMse']")
         # messageBar.send_keys(msg,Keys.ENTER)
     while True:
-        messageBar = driver.find_element_by_xpath("//div[@class='_3uMse _118Ah']")
+        # messageBar = driver.find_element_by_xpath("//div[@class='_3uMse']")
+        # messageBar=driver.find_elements_by_class_name('_3uMse')
+        messageBar=driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
         messageBar.send_keys(msg,Keys.ENTER)
+        count=count+1
+    print(count)
     time.sleep(100)
 
 
